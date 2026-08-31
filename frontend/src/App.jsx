8d851dbf8fae1eaa86b83fbf0a9e15cffd550c30
Emily Chase <166661533+emchase8@users.noreply.github.com>
2026-08-31 14:25:59 -0600

    adding base pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
-import { useLocation, BrowserRouter, Routes, Route, Link } from 'react-dom'
 import Welcome from "./welcome/Welcome.tsx"
 
 
