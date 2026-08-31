40e37d5eca55d4eaf368f7614395c89e806bd76d
Clay Eldredge <clay.eldredge@gmail.com>
2026-08-31 14:34:03 -0600

    Remove non existent dependencies in migration file

--- api/migrations/0001_enable_pgvector.py
+++ api/migrations/0001_enable_pgvector.py
@@ -3,8 +3,7 @@ from pgvector.django import VectorExtension
 
 class Migration(migrations.Migration):
     dependencies = [
-        # Leave whatever dependency Django automatically put here intact
-        ('api', '0001_initial'), 
+
     ]
 
     operations = [
