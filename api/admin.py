4727a865ebcaa748f5396e821387a333f0de5193
Clay Eldredge <clay.eldredge@gmail.com>
2026-08-31 16:01:58 -0600

    Add all models to admin page

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Experience, Education, Skill, Resume, Job
 # Register your models here.
+admin.site.register(Experience)
+admin.site.register(Education)
+admin.site.register(Skill)
+admin.site.register(Resume)
+admin.site.register(Job)
