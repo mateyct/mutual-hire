97a45cdb631bf9d5480cafe5e41bdf92b7af4b94
ansommer <ansommer@byu.edu>
2026-08-31 17:45:30 -0600

    separate menus for company vs applicant

--- frontend/src/pages/Menu.tsx
+++ frontend/src/pages/Menu.tsx
@@ -2,13 +2,22 @@ import  './Menu.css'
 import { slide as Menu} from 'react-burger-menu'
 
 
-const SideMenu = () => {
+const SideMenu = ({ userType }: { userType: string }) => {
 
 return (
     <Menu>
-      <a id="match" className="menu-item" href="/match">Match</a>
-        <a id="account" className="menu-item" href="/about">Account</a>
-        <a id="logout" className="menu-item" href="/">Log Out</a>
+      {userType === "CompanyUser" && [
+        <a key="my-jobs" className="menu-item" href="/my-jobs">My Jobs</a>,
+        <a key="hire-ready" className="menu-item" href="/hire-ready">Hire Ready</a>
+      ]}
+      {userType === "ApplicantUser" && [
+          <a className="menu-item" href="/resume">Resume</a>,
+          <a className="menu-item" href="/match">Match</a>,
+          <a className="menu-item" href="/job-interests">Job Interests</a>,
+      ]}
+      
+      <a id="account" className="menu-item" href="/account">Account</a>
+      <a id="logout" className="menu-item" href="/">Log Out</a>
     </Menu>
 )
 }
