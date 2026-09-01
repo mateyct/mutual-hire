9f6b2a2b131c48d1efb1c6b8f45bda7d422e86c7
Clay Eldredge <clay.eldredge@gmail.com>
2026-08-31 23:03:51 -0600

    Add matching jobs endpoint

--- api/matching.py
+++ api/matching.py
@@ -24,3 +24,21 @@ def find_matching_resumes_for_job(job:Job):
     ).order_by('composite_score')[:10]  # lowest composite score = best match, batches of 10
 
     return ranked_resumes
+
+
+def find_matching_jobs_for_resume(resume: Resume):
+    if not resume.skills_embedding or not resume.experience_embedding or not resume.education_embedding:
+        return []
+
+    ranked_jobs = Job.objects.exclude(description_embedding__isnull=True).annotate(
+        skill_dist=CosineDistance('description_embedding', resume.skills_embedding),
+        exp_dist=CosineDistance('description_embedding', resume.experience_embedding),
+        edu_dist=CosineDistance('description_embedding', resume.education_embedding),
+    ).annotate(
+        composite_score=ExpressionWrapper(
+            (F('skill_dist') * SKILL_WEIGHT) + (F('exp_dist') * EXP_WEIGHT) + (F('edu_dist') * EDU_WEIGHT),
+            output_field=FloatField()
+        )
+    ).order_by('composite_score')[:10]  # lowest composite score = best match, batches of 10
+
+    return ranked_jobs
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import MatchingResumes
+from .views import MatchingResumes, MatchingJobs
 
 from .views import RegisterView
 
@@ -8,5 +8,6 @@ urlpatterns = [
     #path('test/', TestEndpointView.as_view(), name='test-endpoint'),
     path('auth/login/', obtain_auth_token, name='login'),
     path('auth/register/<str:user_type>/', RegisterView.as_view(), name="register"),
-    path('matching/resumes/<int:job_id>', MatchingResumes.as_view(), name='matching_resumes'),
+    path('matching/resumes/', MatchingResumes.as_view(), name='matching_resumes'),
+    path('matching/jobs/', MatchingJobs.as_view(), name='matching_jobs'),
 ]
